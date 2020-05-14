b465218921c6a11616713e573d6ee4f9b4b2045d
Felipe Beltran <felipebeltran.nyc@gmail.com>
2020-05-14 16:34:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,11 @@ class Song
   @@song_count = 0
   attr_accessor :name, :artist, :genre 
   
-  def initialize
+  def initialize(name, artist, genre)
     @@song_count += 1 
+    @name = name
+    @artist = artist
+    @genre = genre
   end
   
   def self.count
